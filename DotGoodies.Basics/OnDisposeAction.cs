c2cb813a1ea4818491e4e4119e254ceb69121e44
Igor Fedurkin <igor.fedurkin@pds.nl>
2015-11-21 21:42:13 +0100

    + on dispose action

---
+++ DotGoodies.Basics/OnDisposeAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DotGoodies.Basics
+{
+    public sealed class OnDisposeAction : IDisposable
+    {
+        public static IDisposable For(Action action)
+        {
+            return new OnDisposeAction(action);
+        }
+
+        private readonly Action _action;
+
+        private OnDisposeAction(Action action)
+        {
+            _action = action;
+        }
+
+        void IDisposable.Dispose()
+        {
+            _action();
+        }
+    }
+}
