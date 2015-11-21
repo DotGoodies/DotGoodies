e97278ca77343bd75d47af23829d01814731e253
Igor Fedurkin <igor.fedurkin@pds.nl>
2015-11-21 22:03:55 +0100

    Range type and an extension method added

---
+++ DotGoodies.Basics/CollectionOfDateTimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotGoodies.Basics
+{
+    public static class CollectionOfDateTimeExtensions
+    {
+        public static IEnumerable<TimeSpan> GetDurations(this IReadOnlyList<DateTime> timestamps, bool absolute)
+        {
+            if(timestamps.Count < 2)
+                throw new InvalidOperationException("Insufficient data.");
+
+            for (int i = 0; i < timestamps.Count - 1; i++)
+            {
+                var duration = timestamps[i + 1] - timestamps[i];
+                if (absolute)
+                    yield return duration.Duration();
+                else
+                    yield return duration;
+            }
+        } 
+    }
+}
---
+++ DotGoodies.Basics/Range.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DotGoodies.Basics
+{
+    public struct Range<T> 
+        where T : struct, IComparable<T>, IEquatable<T>
+    {
+        private readonly T _high;
+        private readonly T _low;
+
+        public Range(T low, T high)
+        {
+            if (low.CompareTo(high) > 0)
+            {
+                throw new ArgumentException(
+                    "Low must be less than or equal to high. Low: {0}, High: {1}".FormatWith(low, high));
+            }
+
+            _low = low;
+            _high = high;
+        }
+
+        public T Low => _low;
+
+        public T High => _high;
+    }
+}
