0e46a7d48fe7ad641597d8ad7b2e42931b900798
Igor Fedurkin <igor.fedurkin@pds.nl>
2015-11-26 01:27:06 +0100

    A couple of new extensions.

---
+++ DotGoodies.Basics/ConcurrentCollectionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace DotGoodies.Basics
+{
+    public static class ConcurrentCollectionsExtensions
+    {
+        public static bool RemoveExisting<TKey, TValue>(
+            this ConcurrentDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue expectedValue)
+        {
+            return ((ICollection<KeyValuePair<TKey, TValue>>)dictionary).Remove(new KeyValuePair<TKey, TValue>(key, expectedValue));
+        }
+
+    }
+}
---
+++ DotGoodies.Basics/GenericExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace DotGoodies.Basics
+{
+    public static class GenericExtensions
+    {
+        public static bool Is<T>(this object obj)
+        {
+            return obj is T;
+        }
+
+        public static bool IsNot<T>(this object obj)
+        {
+            return !obj.Is<T>();
+        }
+    }
+}
